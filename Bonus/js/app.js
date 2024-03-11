2ddfda3a52ef047a18138245ed697084e948dca3
AndreaGiacometti <159144064+AndreaGiacometti@users.noreply.github.com>
2024-03-11 23:09:09 +0100

    FIX improved js app

--- Bonus/js/app.js
+++ Bonus/js/app.js
@@ -8,78 +8,66 @@ console.log(kmInputElement)
 const buttonElement = document.getElementById('submit') //object
 console.log(buttonElement)
 
-// abilitare ascolto clic sul bottone
-// buttonElement.addEventListener('click', function () {
-// 	console.log('click on button')
+// definire elemento form
 const formElement = document.getElementById('ticket-form');
-    formElement.addEventListener('submit', function (event) {
-        // Impedisco che il form venga effettivamente inviato e la pagina si ricarichi
-        event.preventDefault();
 
-// recuperare l'opzione selezionata dal dropdown menu
-const discountList = document.getElementById("discount-list");
-const selectedValue = discountList.value;
+// abilitare ascolto evento 'submit' sul form
+formElement.addEventListener('submit', function (event) {
+    // impedire che il form venga effettivamente inviato e la pagina si ricarichi
+    event.preventDefault();
 
-// chiedere il numero dei km da percorrere
-const km = parseFloat(kmInputElement.value); //number
-//console.log(km)
+    // recuperare l'opzione selezionata dal dropdown menu
+    const discountList = document.getElementById("discount-list");
+    const selectedValue = discountList.value;
 
-// validare i dati
-// - km > 0
-console.log(km > 0)
-// - km non è NaN
-console.log(!isNaN(km))
-//selectedValue != "0" è diverso da 0
-console.log(selectedValue != "0")
+    // chiedere il numero dei km da percorrere
+    const km = parseFloat(kmInputElement.value); //number
 
-//Se i dati sono validi
-if (km > 0 && !isNaN(km) && selectedValue != "0") {
+    // validare i dati
+        // km > 0
+        console.log(km > 0)
+        // km non è NaN
+        console.log(!isNaN(km))
+        //selectedValue != "0"
+        console.log(selectedValue != "0")
 
-// impostare variabile prezzo al km
-const pricePerKm = 0.21; //number
-
-// calcolare il prezzo del biglietto senza sconti
-const basePrice = (km * pricePerKm); //number
-//console.log(basePrice)
-
-// impostare le variabili di sconto
-    if (selectedValue === "1") {
-        discount = 0.2;
-    } else if (selectedValue === "2") {
-        discount = 0.4;
-    } else if (selectedValue === "3") {
-       discount = 0;
-    } 
-
-// calcolare il prezzo del biglietto applicando gli sconti
-const price = (basePrice - basePrice * discount) //number
-
-// stampare il prezzo del biglietto sulla viewport tramite collegamento a html
-
-
-if (selectedValue === "1") {
-    shownDiscount = 20;
-} else if (selectedValue === "2") {
-    shownDiscount = 40;
-} else if (selectedValue === "3") {
-   shownDiscount = 0;
-}
-
-const kmElement = document.getElementById('km-percorsi')
-kmElement.innerHTML = km+'km'
-
-const basePriceElement = document.getElementById('base-price')
-basePriceElement.innerHTML = basePrice.toFixed(2) + '&euro;'
-
-const discountElement = document.getElementById('discount')
-discountElement.innerHTML = shownDiscount + '%'
-
-const priceElement = document.getElementById('price')
-priceElement.innerHTML = price.toFixed(2) + '&euro;'
-
-
-} else {
-    //ALTRIMENTI 
-    alert('I dati inseriti non sono validi e/o nessuna opzione di sconto selezionata')
-}
+    //SE i dati sono validi
+    if (km > 0 && !isNaN(km) && selectedValue != "0") {
+        // impostare variabile prezzo al km
+        const pricePerKm = 0.21; //number
+        
+        // calcolare il prezzo del biglietto senza sconti
+        const basePrice = (km * pricePerKm); //number
+        
+        // impostare le variabili di sconto
+        if (selectedValue === "1") {
+            discount = 0.2;
+            shownDiscount = 20;
+        } else if (selectedValue === "2") {
+            discount = 0.4;
+            shownDiscount = 40;
+        } else if (selectedValue === "3") {
+            discount = 0; 
+            shownDiscount = 0;
+        } 
+        
+        // calcolare il prezzo del biglietto applicando gli sconti
+        const price = (basePrice - basePrice * discount) //number
+        
+        // stampare il prezzo del biglietto sulla viewport tramite collegamento a html
+        const kmElement = document.getElementById('km-percorsi')
+        kmElement.innerHTML = km+'km'
+        
+        const basePriceElement = document.getElementById('base-price')
+        basePriceElement.innerHTML = basePrice.toFixed(2) + '&euro;'
+        
+        const discountElement = document.getElementById('discount')
+        discountElement.innerHTML = shownDiscount + '%'
+        
+        const priceElement = document.getElementById('price')
+        priceElement.innerHTML = price.toFixed(2) + '&euro;'
+    } else {
+        //ALTRIMENTI 
+        alert('I dati inseriti non sono validi e/o nessuna opzione di sconto selezionata')
+    }
 })
